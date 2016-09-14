5ba5203b4776fc52b7458e6047db22056820d3bc
james abel <j@abel.co>
2016-09-14 11:36:36 -0700

    source readme is .rst

--- make_dist.sh
+++ make_dist.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #
 # copy over the files that are used for other repos to formats that setup wants
-pandoc --from=markdown --to=rst --output=README.rst readme.md
+pandoc --from=rst --to=markdown --output=readme.md readme.rst
+pandoc --from=rst --to=plain --output=readme.txt readme.rst
 cp LICENSE LICENSE.txt
 python3 setup.py sdist
 #
