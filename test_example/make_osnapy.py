61dbed769130d1ec050a302171ee2a13bc4a777b
james abel <j@abel.co>
2016-09-26 23:31:46 -0700

    new calling convention

--- test_example/make_osnapy.py
+++ test_example/make_osnapy.py
@@ -1,19 +1,13 @@
 
 import osnap.osnapy
-import osnap.util
+
+import test_example
 
 
 def create_osnapy(verbose):
 
-    # we currently have slightly different versions across the OSs
-    if osnap.util.is_windows():
-        python_version = '3.5.2'
-    elif osnap.util.is_mac():
-        python_version = '3.5'
-    else:
-        raise NotImplementedError
-
-    osnap.osnapy.make_osnapy(python_version, verbose=verbose)
+    osnap.osnapy.make_osnapy(test_example.__python_version__, application_name=test_example.__application_name__,
+                             force_app_uninstall=True, verbose=verbose)
 
 
 if __name__ == '__main__':
