09f29ba8fee445856298948b635b19090ade84e4
james abel <j@abel.co>
2016-09-27 14:18:46 -0700

    initial check-in

---
+++ test_example/test_run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# set -x
+pushd .
+cd ../test_example
+set -x
+rm -rf .eggs build dist logs venv || { echo 'could not rm -rf : re-run with sudo' ; exit 1;  }
+set +x
+./make_venv.sh
+./install_osnap.sh
+./make_osnapy.sh
+./make_installer.sh
+popd
