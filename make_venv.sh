480e393bd9a0c570695914394b9c80fa6b7e5bde
james abel <j@abel.co>
2016-09-27 11:04:59 -0700

    normal python path

--- make_venv.sh
+++ make_venv.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 #
-# point this to your python 3
-# we had multiples on our system so we had to be very specific
-if [ -z "$MYPYTHONHOME" ]; then
-    export MYPYTHONHOME=/usr/local/Cellar/python3/3.5.2_1
-fi
-echo ${MYPYTHONHOME}
-${MYPYTHONHOME}/bin/pyvenv --clear venv
+/usr/local/bin/pyvenv --clear venv
+./venv/bin/pip3 install -U pip
 ./venv/bin/pip3 install -U -r requirements.txt
