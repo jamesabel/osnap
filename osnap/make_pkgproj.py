db9a06e5c97d56d2cc26bc3aea5006ea8c1b7e1c
james abel <j@abel.co>
2016-09-27 14:16:17 -0700

    new finder for template.pkgproj

--- osnap/make_pkgproj.py
+++ osnap/make_pkgproj.py
@@ -1,14 +1,29 @@
 import os
 import sys
+import site
 
 import osnap.const
+import osnap.util
 
 from jinja2 import Template
 
 
 def make_prkproj(application_name, pkgproj_path, verbose):
-    template_file_path = os.path.join(os.path.dirname(sys.executable), '..', osnap.const.package_name,
-                                      'template.pkgproj')
+
+    # find Packages project file
+    template_file = 'template.pkgproj'
+    locations = []
+    for d in site.getsitepackages():
+        for r, _, fs in os.walk(d):
+            for f in fs:
+                if f == template_file:
+                    locations.append(os.path.join(r, f))
+    if len(locations) != 1:
+        s = 'error : looking for exactly one %s : found %s' % (template_file, str(locations))
+        print(s)
+        sys.exit(s)
+    template_file_path = locations[0]
+
     if verbose:
         print('using %s as template' % template_file_path)
     with open(template_file_path) as template_file:
