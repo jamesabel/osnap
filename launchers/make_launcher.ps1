9123c2dc0ed60b5e4ddd82a85762506a618c4eca
Eli Ribble <eli@authentise.com>
2016-11-07 13:27:49 -0800

    Fix writing the launch name by architecture
    I didn't like having AMD64 and x86, so I made the mapping more explicit.
    I'm also moving the original architecture launcher to the correct name for
    its architecture

--- launchers/make_launcher.ps1
+++ launchers/make_launcher.ps1
@@ -5,7 +5,12 @@ Write-Host "Building launcher with py2exe"
 & python.exe setup.py py2exe
 Write-Host "Zipping up launcher"
 & python.exe dist2zip.py
-$OUTPUT = "launchwin-$ENV:PROCESSOR_ARCHITECTURE.zip"
+switch ($ENV:PROCESSOR_ARCHITECTURE)
+{
+	"AMD64" {$OUTPUT = "launchwin-amd64.zip"}
+	"x86"	{$OUTPUT = "launchwin-x86.zip"}
+	default {$OUTPUT = "launchwin-unknown.zip"}
+}
 Write-Host "Copying launcher to expected location at ..\osnap\$OUTPUT"
 Copy-Item launchwin.zip ..\osnap\$OUTPUT
 
