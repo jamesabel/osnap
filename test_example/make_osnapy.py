63ad7b924b491fe6af8b84b866b72cd54fce7582
james abel <j@abel.co>
2016-11-10 22:20:44 -0800

    no longer using verbose as a parameter

--- test_example/make_osnapy.py
+++ test_example/make_osnapy.py
@@ -7,7 +7,7 @@ import test_example
 def create_osnapy(verbose):
 
     osnap.osnapy.make_osnapy(test_example.__python_version__, application_name=test_example.__application_name__,
-                             force_app_uninstall=True, verbose=verbose)
+                             force_app_uninstall=True)
 
 
 if __name__ == '__main__':
