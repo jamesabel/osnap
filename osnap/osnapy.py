89641939fe0b2aacb6c1e900e143e7ba909a3712
james abel <j@abel.co>
2016-12-05 18:33:08 -0800

    add version checking

--- osnap/osnapy.py
+++ osnap/osnapy.py
@@ -1,6 +1,7 @@
 
 import argparse
 import logging
+import requests
 
 import osnap.osnapy_win
 import osnap.osnapy_mac
@@ -18,6 +19,15 @@ def make_osnapy(
         force_app_uninstall     = False,
         architecture            = '64bit',
         ):
+
+    LOGGER.info('your osnap version : %s' % osnap.__version__)
+    r = requests.get('http://api.abel.co/osnap/version')
+    if r and r.status_code == 200:
+        pypi_version = r.text
+        LOGGER.info('PyPI osnap version : %s' % pypi_version)
+        if tuple(pypi_version.split('.')) > tuple(osnap.__version__.split('.')):
+            print('note: the latest osnap version is %s, your version is %s' % (pypi_version, osnap.__version__))
+
     LOGGER.debug('creating osnapy Python environment using python %s' % python_version)
     if osnap.util.is_mac() and application_name is None:
         raise Exception('must specify the application name on mac')
