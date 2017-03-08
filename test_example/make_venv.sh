7f2039660258b31ff0960f70dfc631e7b40b3fa2
james abel <j@abel.co>
2017-03-08 12:23:17 -0800

    update venv technique

--- test_example/make_venv.sh
+++ test_example/make_venv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-/usr/local/bin/pyvenv --clear venv
+python3.6 -m venv --clear venv
 ./venv/bin/pip3 install -U pip
 ./venv/bin/pip3 install -U setuptools
 ./venv/bin/pip3 install -U -r requirements.txt
