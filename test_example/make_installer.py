73017f18b8c42dbfb05117a9da6d4825734fbcf0
james abel <j@abel.co>
2017-03-13 10:40:49 -0700

    add version

--- test_example/make_installer.py
+++ test_example/make_installer.py
@@ -4,13 +4,12 @@ import osnap.installer
 import test_example
 
 
-def make_installer(verbose):
-    osnap.installer.make_installer(test_example.__python_version__, test_example.__version__,
-                                   test_example.__application_name__,
-                                   test_example.__author__, 'this is my test example', 'www.abel.co',
-                                   variant='window'
+def make_installer():
+    osnap.installer.make_installer(test_example.__python_version__, test_example.__application_name__,
+                                   test_example.__version__, test_example.__author__, 'this is my test example',
+                                   'www.abel.co', variant='window'
                                    )
 
 
 if __name__ == '__main__':
-    make_installer(True)
+    make_installer()
